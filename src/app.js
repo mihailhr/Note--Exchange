5a5f15a0d90a47b1fbfb372230603dc336d62ee5
mihailhr <mishohristov3@gmail.com>
2024-10-21 15:08:15 +0300

    View engine setup

--- src/app.js
+++ src/app.js
@@ -1,12 +1,24 @@
 const express=require("express")
+const {engine}=require("express-handlebars")
+const path=require("path")
+
+
+
+
 
 const app=express()
-
+app.engine('handlebars',engine())
+app.set('view engine', 'handlebars')
+app.set('views', path.join(__dirname,'views'))
 const port=4000
 
 app.get("/",(req,res)=>{
+    try {
+        res.status(200).render("home")
+    } catch (error) {
+        res.status(500).send("An error ocurred while rendering this page.")
+    }
     
-    res.status(200).send("OK")
 })
 
 try {
